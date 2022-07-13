65a38f6c14bad4167af9a4f132cf1a66ece39f21
marauder77 <63757565+marauder77@users.noreply.github.com>
2022-07-12 19:28:15 -0700

    updated for part 2

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ db.create_all()
 def test_page():
     return render_template("/users")
 
+@app.route('404')
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 @app.route('/users')
 # Show an add form for users
@@ -83,3 +87,59 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def posts_new_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def posts_new(user_id):
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
--- models.py
+++ models.py
@@ -12,14 +12,29 @@ class User(db.Model):
     __tablename__ = "users"
 # NOT SURE SURE WHAT IS GOING ON HERE, COPIED FROM SOLUTION AND ArgumentError appears
     id = db.Column(int, primary_key=True)
-    first_name = db.Column(text, nullable=False)
-    last_name = db.Column(text, nullable=False)
-    image_url = db.Column(text, nullable=False, default=DEFAULT_IMAGE_URL)
+    first_name = db.Column(db.Interger, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
 
     @property
     def full_name(self):
 
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Interger, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=Fasle)
+    created_at = db.Column(db.DateTime, nullable=False, default = datetime.datetime.now)
+    user_id = db.Column(db.Interger, db.ForeignKey('users.id'), nullable=False)
+
+
 
 def connect_db(app):
     """Connect this database to provided Flask app.
